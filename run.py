dcf5a7ad889f6220adfd8f70fadbd051696d35e8
Shiaba <>
2023-09-12 18:57:16 +0000

    Add guess input on while loop with if/else choices

--- run.py
+++ run.py
@@ -1,8 +1,20 @@
 import random
 
-
 '''
 Generate random numbers between 1-100 in the game.
 '''
-random_number = random.randint(1,100)
-print(random_number)
+random_number = random.randint(1, 100)
+guess = None
+
+while guess != random_number:
+    guess = int(input('Enter number between 1-100: '))
+
+    if (guess < random_number):
+        print('Number is higher than your guess')
+
+    elif (guess > random_number):
+        print('Number is lower than your guess')
+
+    else:
+        print('Congrats, You guessed correctly!')
+        break
