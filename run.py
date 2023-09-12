b1ba2995096a75696d37a478c1c74123f0adb06d
Shiaba <>
2023-09-12 20:21:15 +0000

    Add function to while statement

--- run.py
+++ run.py
@@ -4,17 +4,22 @@ import random
 Generate random numbers between 1-100 in the game.
 '''
 random_number = random.randint(1, 100)
-guess = None
 
-while guess != random_number:
-    guess = int(input('Enter number between 1-100: '))
+'''
+Guess a number and the if/else statements will calculate
+whether it is too high, low or correct answer.
+'''
+def guess_number():
+    guess = None
+    while guess != random_number:
+        guess = int(input('Enter number between 1-100: '))
 
-    if (guess < random_number):
-        print('Number is higher than your guess')
+        if (guess < random_number):
+            print('Number is higher than your guess')
 
-    elif (guess > random_number):
-        print('Number is lower than your guess')
+        elif (guess > random_number):
+            print('Number is lower than your guess')
 
-    else:
-        print('Congrats, You guessed correctly!')
-        break
+        else:
+            print('Congrats, You guessed correctly!')
+            break
