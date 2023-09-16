b786aba2c10d78c3b8d19343c4fbb69faf3c297b
Shiaba <>
2023-09-16 14:40:51 +0000

    Add try/except statement for if user inputs text or value above or below intended game value

--- run.py
+++ run.py
@@ -15,28 +15,39 @@ whether it is too high, low or correct answer.
 def guess_number():
     guesses = 0
     while guesses < 3:
-        print(f'Times guessed: {guesses}')
+        print(f'Times guessed: {guesses}\n')
 
-        guess = int(input('Enter number between 1-100: '))
+        try:
+            guess = int(input(f'Enter a number between {min_value} & {max_value}: \n'))
+
+            if guess < min_value:
+                print('Below 1\n')
+                continue
+            elif guess > max_value:
+                print('Above 100\n')
+                continue
+        except:
+            print(f'Error! Write a number between {min_value} & {max_value}.\n')
+            continue
 
         if guess < random_number:
-            print('Number is higher than your guess')
+            print('Number is higher than your guess\n')
 
         elif guess > random_number:
-            print('Number is lower than your guess')
+            print('Number is lower than your guess\n')
 
         else:
-            print('Congrats, You guessed correctly!')
+            print('Congrats, You guessed correctly!\n')
             break
 
         guesses = guesses + 1
 
 
 def start():
-    name = input('Write your name here: ')
-    print(f'Hello {name}, good luck!')
+    name = input('Write your name here: \n')
+    print(f'Hello {name}, good luck!\n')
     guess_number()
 
 
-print('Add welcome text & rules here')
+print('Add welcome text & rules here\n')
 start()
